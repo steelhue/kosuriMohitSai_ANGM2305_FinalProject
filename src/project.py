01403a2037730d6126a708abc63eed0fcbfd30d2
Mohit Sai Kosuri <mohitsai.doodle@gmail.com>
2026-04-24 11:28:11 -0500

    Made few changes in selected_edges_to_group mthod where it checks if the vector is zero or not

--- src/project.py
+++ src/project.py
@@ -1,5 +1,4 @@
 import maya.cmds as cmds
-import math
 
 
 # group edges based on the angle
@@ -18,7 +17,7 @@ def get_edge_vector(edge):
         # let P and Q be the start and end points of an edge
         # vector PQ = Q - P
     verts = cmds.polyListComponentConversion(edge, toVertex=True)
-    verts = cmds.ls(verts, flatter=True)
+    verts = cmds.ls(verts, flatten=True)
 
     p = cmds.xform(verts[0], q=True, ws=True, t=True)
     q = cmds.xform(verts[1], q=True, ws=True, t=True)
@@ -27,18 +26,28 @@ def get_edge_vector(edge):
     return vector
 
 
+def is_zero_vector(v):
+    return v[0] == 0.0 and v[1] == 0.0 and v[2] == 0.0
+
+
 def selected_edges_to_groups(corner_angle_threshold=45.0):
     selected_edges = cmds.ls(selection=True, flatten=True)
 
     edge_groups = [[selected_edges[0]]]
-    idx = 0
     current_group = 0
 
     for idx in range(len(selected_edges) - 1):
         vec_a = get_edge_vector(selected_edges[idx])
         vec_b = get_edge_vector(selected_edges[idx + 1])
 
-        angle_btw = cmds.angleBetween(vec_a, vec_b)
+        if is_zero_vector(vec_a) or is_zero_vector(vec_b):
+            edge_groups[current_group].append(selected_edges[idx + 1])
+            continue
+
+        angle_btw = cmds.angleBetween(
+            vector1=[vec_a[0], vec_a[1], vec_a[2]],
+            vector2=[vec_b[0], vec_b[1], vec_b[2]]
+        )
 
         angle = angle_btw[3]
 
